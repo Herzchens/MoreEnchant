eac38c18381a13ac1b0b69d029f4a9d02909ba93
Herzchens <Herzchen273@gmail.com>
2025-09-13 22:46:48 +0700

    Update PermissionManager to enhance performance

--- src/main/kotlin/com/herzchen/moreenchant/manager/PermissionManager.kt
+++ src/main/kotlin/com/herzchen/moreenchant/manager/PermissionManager.kt
@@ -1,18 +1,14 @@
 package com.herzchen.moreenchant.manager
 
 import com.herzchen.moreenchant.MoreEnchant
-
-import org.bukkit.Material
 import org.bukkit.entity.Player
 
 class PermissionManager(private val configManager: ConfigManager) {
     constructor(plugin: MoreEnchant) : this(plugin.configManager)
 
-    fun getBestDropGroup(player: Player): Map<Material, Double> {
+    fun getBestDropGroup(player: Player): ConfigManager.DropGroup? {
         return configManager.dropGroups.values
             .firstOrNull { it.permission.isEmpty() || player.hasPermission(it.permission) }
-            ?.drops
-            ?: configManager.dropGroups["default"]?.drops
-            ?: emptyMap()
+            ?: configManager.dropGroups["default"]
     }
 }

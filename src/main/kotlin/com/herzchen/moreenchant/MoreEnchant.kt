4ed4f1677cf0731f0f2b1751b307c57880691b3e
Herzchens <Herzchen273@gmail.com>
2025-09-15 18:15:52 +0700

    Register Smelting enchantment, change ASCII art to Rainbow, add logger

--- src/main/kotlin/com/herzchen/moreenchant/MoreEnchant.kt
+++ src/main/kotlin/com/herzchen/moreenchant/MoreEnchant.kt
@@ -1,13 +1,13 @@
 package com.herzchen.moreenchant
 
 import com.herzchen.moreenchant.commands.MoeCommand
-import com.herzchen.moreenchant.utils.TabCompleter
+import com.herzchen.moreenchant.enchantments.Smelting
 import com.herzchen.moreenchant.enchantments.VirtualExplosion
 import com.herzchen.moreenchant.integration.ExtraStorageHook
 import com.herzchen.moreenchant.listener.BlockBreakListener
-import com.herzchen.moreenchant.manager.*
 import com.herzchen.moreenchant.listener.PlayerItemChangeListener
-
+import com.herzchen.moreenchant.manager.*
+import com.herzchen.moreenchant.utils.TabCompleter
 import org.bukkit.event.HandlerList
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -16,33 +16,35 @@ class MoreEnchant : JavaPlugin() {
     lateinit var enchantManager: EnchantManager
     lateinit var permissionManager: PermissionManager
     lateinit var extraStorageHook: ExtraStorageHook
-    lateinit var virtualExplosion: VirtualExplosion
     lateinit var bossBarManager: BossBarManager
     lateinit var performanceOptimizer: PerformanceOptimizer
 
-    private val enableArt = """
-        
-         /@@      /@@           /@@@@@@@@          
-        | @@@    /@@@          | @@_____/          
-        | @@@@  /@@@@  /@@@@@@ | @@       /@@@@@@@ 
-        | @@ @@/@@ @@ /@@__  @@| @@@@@   | @@__  @@
-        | @@  @@@| @@| @@  \ @@| @@__/   | @@  \ @@
-        | @@\  @ | @@| @@  | @@| @@      | @@  | @@
-        | @@ \/  | @@|  @@@@@@/| @@@@@@@@| @@  | @@
-        |__/     |__/ \______/ |________/|__/  |__/
-    """.trimIndent()
+    var virtualExplosion: VirtualExplosion? = null
+    var smelting: Smelting? = null
 
-    private val disableArt = """
-        
-         /@@      /@@           /@@@@@@@@          
-        | @@@    /@@@          | @@_____/          
-        | @@@@  /@@@@  /@@@@@@ | @@       /@@@@@@@ 
-        | @@ @@/@@ @@ /@@__  @@| @@@@@   | @@__  @@
-        | @@  @@@| @@| @@  \ @@| @@__/   | @@  \ @@
-        | @@\  @ | @@| @@  | @@| @@      | @@  | @@
-        | @@ \/  | @@|  @@@@@@/| @@@@@@@@| @@  | @@
-        |__/     |__/ \______/ |________/|__/  |__/
-    """.trimIndent()
+    private fun showEnableArt() {
+        this.server.consoleSender.sendMessage("")
+        this.server.consoleSender.sendMessage("§c         /@@      /@@           /@@@@@@@@          ")
+        this.server.consoleSender.sendMessage("§6        | @@@    /@@@          | @@_____/          ")
+        this.server.consoleSender.sendMessage("§e        | @@@@  /@@@@  /@@@@@@ | @@       /@@@@@@@ ")
+        this.server.consoleSender.sendMessage("§a        | @@ @@/@@ @@ /@@__  @@| @@@@@   | @@__  @@")
+        this.server.consoleSender.sendMessage("§b        | @@  @@@| @@| @@  \\ @@| @@__/   | @@  \\ @@")
+        this.server.consoleSender.sendMessage("§d        | @@\\  @ | @@| @@  | @@| @@      | @@  | @@")
+        this.server.consoleSender.sendMessage("§5        | @@ \\/  | @@|  @@@@@@/| @@@@@@@@| @@  | @@")
+        this.server.consoleSender.sendMessage("§9        |__/     |__/ \\______/ |________/|__/  |__/")
+    }
+
+    private fun showDisableArt() {
+        this.server.consoleSender.sendMessage("")
+        this.server.consoleSender.sendMessage("§8         /@@      /@@           /@@@@@@@@          ")
+        this.server.consoleSender.sendMessage("§7        | @@@    /@@@          | @@_____/          ")
+        this.server.consoleSender.sendMessage("§c        | @@@@  /@@@@  /@@@@@@ | @@       /@@@@@@@ ")
+        this.server.consoleSender.sendMessage("§4        | @@ @@/@@ @@ /@@__  @@| @@@@@   | @@__  @@")
+        this.server.consoleSender.sendMessage("§c        | @@  @@@| @@| @@  \\ @@| @@__/   | @@  \\ @@")
+        this.server.consoleSender.sendMessage("§7        | @@\\  @ | @@| @@  | @@| @@      | @@  | @@")
+        this.server.consoleSender.sendMessage("§8        | @@ \\/  | @@|  @@@@@@/| @@@@@@@@| @@  | @@")
+        this.server.consoleSender.sendMessage("§0        |__/     |__/ \\______/ |________/|__/  |__/")
+    }
 
     override fun onEnable() {
         configManager = ConfigManager(this)
@@ -50,19 +52,41 @@ class MoreEnchant : JavaPlugin() {
         enchantManager = EnchantManager()
         bossBarManager = BossBarManager()
         extraStorageHook = ExtraStorageHook(this)
-        virtualExplosion = VirtualExplosion(this)
+
+        if (configManager.virtualExplosionEnabled) {
+            virtualExplosion = VirtualExplosion(this)
+            logger.info("VirtualExplosion đã được kích hoạt")
+        }
+
+        if (configManager.smeltingEnabled) {
+            smelting = Smelting(this)
+            logger.info("Smelting đã được kích hoạt")
+        }
+
         performanceOptimizer = PerformanceOptimizer(this)
+
         server.pluginManager.registerEvents(PlayerItemChangeListener(this), this)
-
-
         server.pluginManager.registerEvents(BlockBreakListener(this), this)
-        getCommand("moe")?.setExecutor(MoeCommand(this))
-        getCommand("moe")?.tabCompleter = TabCompleter(configManager)
 
-        this.server.consoleSender.sendMessage("§b$enableArt")
+        getCommand("moe")?.setExecutor(MoeCommand(this))
+        getCommand("moe")?.tabCompleter = TabCompleter(this)
+
+        showEnableArt()
         this.server.consoleSender.sendMessage("§a================================================================================")
         this.server.consoleSender.sendMessage("§e>> MoreEnchant v1.1 Đã bật!")
         this.server.consoleSender.sendMessage("§e>> Chạy trên phiên bản Minecraft ${server.version}")
+
+        if (configManager.virtualExplosionEnabled) {
+            this.server.consoleSender.sendMessage("§e>> VirtualExplosion: Đã kích hoạt")
+        } else {
+            this.server.consoleSender.sendMessage("§c>> VirtualExplosion: Đã tắt")
+        }
+
+        if (configManager.smeltingEnabled) {
+            this.server.consoleSender.sendMessage("§e>> Smelting: Đã kích hoạt")
+        } else {
+            this.server.consoleSender.sendMessage("§c>> Smelting: Đã tắt")
+        }
 
         checkWorldGuard()
 
@@ -71,7 +95,7 @@ class MoreEnchant : JavaPlugin() {
             for (player in server.onlinePlayers) {
                 val item = player.inventory.itemInMainHand
                 if (this@MoreEnchant.enchantManager.getEnchantShape(item) != null) {
-                    val isPaused = virtualExplosion.shouldPauseExplosion(player)
+                    val isPaused = virtualExplosion?.shouldPauseExplosion(player) ?: false
                     if (isPaused) {
                         bossBarManager.showBossBar(player,
                             "§eNổ ảo đã bị tạm dừng, vui lòng dọn dẹp item xung quanh bạn!",
@@ -89,7 +113,7 @@ class MoreEnchant : JavaPlugin() {
     override fun onDisable() {
         bossBarManager.removeAllBossBars()
         HandlerList.unregisterAll(this)
-        this.server.consoleSender.sendMessage("§c$disableArt")
+        showDisableArt()
         this.server.consoleSender.sendMessage("§c================================================================================")
         this.server.consoleSender.sendMessage("§c>> MoreEnchant Đã Tắt!")
         logger.info("MoreEnchant đã tắt!")

a6732a2424dac1701b0966e54dbd09f175150a51
Herzchens <Herzchen273@gmail.com>
2025-09-15 18:18:24 +0700

    Update helper

--- src/main/kotlin/com/herzchen/moreenchant/utils/Helper.kt
+++ src/main/kotlin/com/herzchen/moreenchant/utils/Helper.kt
@@ -3,7 +3,6 @@ package com.herzchen.moreenchant.utils
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
-
 import org.bukkit.command.CommandSender
 
 object Helper {
@@ -16,15 +15,19 @@ object Helper {
             .color(NamedTextColor.YELLOW)
             .append(Component.text(" - Hiển thị trang này").color(NamedTextColor.GRAY))
 
-        val line2 = Component.text("/moe enchant <enchantment> [level] ")
+        val line2 = Component.text("/moe enchant virtualexplosion <level> ")
             .color(NamedTextColor.YELLOW)
-            .append(Component.text(" - Thêm phù phép vào vật phẩm đang cầm").color(NamedTextColor.GRAY))
+            .append(Component.text(" - Thêm phù phép nổ ảo").color(NamedTextColor.GRAY))
 
-        val line3 = Component.text("/moe disenchant [enchantment] ")
+        val line3 = Component.text("/moe enchant smelting <level> ")
             .color(NamedTextColor.YELLOW)
-            .append(Component.text(" - Gỡ phù phép khỏi vật phẩm đang cầm").color(NamedTextColor.GRAY))
+            .append(Component.text(" - Thêm phù phép nung chảy").color(NamedTextColor.GRAY))
 
-        val line4 = Component.text("/moe reload ")
+        val line4 = Component.text("/moe disenchant [enchantment] ")
+            .color(NamedTextColor.YELLOW)
+            .append(Component.text(" - Gỡ phù phép").color(NamedTextColor.GRAY))
+
+        val line5 = Component.text("/moe reload ")
             .color(NamedTextColor.YELLOW)
             .append(Component.text(" - Tải lại cấu hình").color(NamedTextColor.GRAY))
 
@@ -33,5 +36,6 @@ object Helper {
         sender.sendMessage(line2)
         sender.sendMessage(line3)
         sender.sendMessage(line4)
+        sender.sendMessage(line5)
     }
 }
